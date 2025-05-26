4150c629b95f5a7cb7892ccc570a9d4db93d8904
chikchicly <chikchicly@gmail.com>
2025-05-26 14:44:13 +0700

    fast-site

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>who is faster PHP or JS _PHP</title>
+    <link rel="stylesheet" href="who-is-faster-PHP-or-JS/public/index.css">
+    <link rel="shortcut icon" href="who-is-faster-PHP-or-JS/public/icon.png" type="image/x-icon">
+</head>
+<body>
+    <main>
+        <header>
+            <?php include("include/header.html"); ?>
+        </header>
+        <article>
+            <?php include("include/article.html"); ?>
+        </article>
+        <footer>
+            <?php include("include/footer.html"); ?>
+        </footer>
+    </main>
+
+</body>
+</html>
